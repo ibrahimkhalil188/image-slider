46faaf98e3194dbbfca8c08357fad5c4924989d9
unknown <ibrahimkhalil.mine@gmail.com>
2022-03-12 10:02:19 +0600

    done

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 const images = [
     "img/5-Shab-e-Barat-Wishes-1.jpg",
-    "img/75918306.webp",
-    "img/1618155691_shutterstock_1939809109.webp",
     "img/al-aqsa-kaaba.png",
-    "img/new-year.jpg",
-    "img/Eid-al-Adha-small-shutterstock_1129968065.webp",
     "img/Jashan-Eid-Milad-Nabi-Mubarak-Greetings.jpg",
+    "img/new-year.jpg",
+    "img/1618155691_shutterstock_1939809109.webp",
+    "img/Eid-al-Adha-small-shutterstock_1129968065.webp",
     "img/lailatul_qadr_4x.webp"
-
 ]
 const imgContainer = document.getElementById("img-slider")
 let imageNumber = 0
 setInterval(function () {
-    const imgUrl = images[imageNumber]
+
     if (imageNumber >= images.length) {
         imageNumber = 0
     }
+    const imgUrl = images[imageNumber]
+    console.log(imgUrl)
     imgContainer.setAttribute("src", imgUrl)
-    console.log(images[imageNumber])
     imageNumber++
 }, 1500)
