ad210dd858296c07f030888e59847bcb0f753861
unknown <ibrahimkhalil.mine@gmail.com>
2022-03-12 09:31:18 +0600

    new imga aded

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const images = [
     "img/75918306.webp",
     "img/1618155691_shutterstock_1939809109.webp",
     "img/al-aqsa-kaaba.png",
-    "img/new-year.jpg"
+    "img/new-year.jpg",
+    "img/Eid-al-Adha-small-shutterstock_1129968065.webp",
+    "img/Jashan-Eid-Milad-Nabi-Mubarak-Greetings.jpg",
+    "img/lailatul_qadr_4x.webp"
+
 ]
 const imgContainer = document.getElementById("img-slider")
 let imageNumber = 0
 setInterval(function () {
     const imgUrl = images[imageNumber]
-    if (imageNumber >= 4) {
+    if (imageNumber >= images.length) {
         imageNumber = 0
     }
     imgContainer.setAttribute("src", imgUrl)
